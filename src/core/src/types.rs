d8010a5afadfa5ed759954fba4d700795287743f
kehiy <me@kehiy.net>
2025-12-11 20:37:41 +0330

    fix: clippy error

--- src/core/src/types.rs
+++ src/core/src/types.rs
@@ -7,7 +7,7 @@ pub type WoTGraph<N> = petgraph::graph::Graph<N, Relation, petgraph::Directed>;
 
 impl Relation {
     pub fn new(name: impl Into<String>, weight: f64) -> Result<Self, String> {
-        if !(weight >= -1.0 && weight <= 1.0) {
+        if !(-1.0..=1.0).contains(&weight) {
             return Err(format!(
                 "Weight must be between -1.0 and 1.0, got {}",
                 weight
